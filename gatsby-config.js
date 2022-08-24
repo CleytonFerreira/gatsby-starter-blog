6d703a07bdabdd26eb5a3d13203abc9760a781cf
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2022-08-24 00:02:58 -0300

    Recreating from scratch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://gatsbystarterblog77407.gatsbyjs.io/`,
   },
-  plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
-      }
-    },
-    `gatsby-transformer-sharp`,
-  ],
-};
+  plugins: [],
+}
