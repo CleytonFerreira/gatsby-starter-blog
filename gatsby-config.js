488c223ba93f9e6d0a4304436f9ddaf42caecaca
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2022-08-25 17:32:41 -0300

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
+    title: "My Super Cool Blog",
     siteUrl: `https://gatsbystarterblog77407.gatsbyjs.io/`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
